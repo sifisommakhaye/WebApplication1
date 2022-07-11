dc1f5964f59625f295d5a5ada9b08205d78d9e18
Sifiso Makhaye <sifiso.makhaye@sage.com>
2022-07-11 14:37:46 +0200

    Add project files.

---
+++ WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "get";
+        }
+
+        public void Add()
+        {
+           
+        }
+
+        public void Subtract()
+        {
+        
+        }
+
+        public void Mutlipy()
+        {
+
+        }
+
+        public void Divide()
+        {
+
+        }
+    }
+}
